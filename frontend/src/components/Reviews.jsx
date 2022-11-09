d5f8a5751f137e2c326b40aff885569f5c7eff0b
timoteahu <51691041+timoteahu@users.noreply.github.com>
2022-11-08 20:59:16 -0800

    c

--- frontend/src/components/Reviews.jsx
+++ frontend/src/components/Reviews.jsx
@@ -11,45 +11,7 @@ function Reviews() {
 
       <div className="grid grid-cols-3 gap-4 w-full" style={{ maxWidth: "80rem" }}>
         <div>
-          <div className="flex flex-col justify-center items-center bg-blue-100 rounded p-6 mb-4">
-            <p >"HELLO!"</p>
-            <p>WLECOME TO THE JUNGLE</p>
-          </div>
-
-          <div className="flex flex-col justify-center items-center bg-blue-100 rounded p-6 mb-4">
-            <p >"HELLO!"</p>
-            <p>WLECOME TO THE JUNGLE</p>
-          </div>
-
-          <div className="flex flex-col justify-center items-center bg-blue-100 rounded p-6 mb-4">
-            <p >"HELLO!"</p>
-            <p>WLECOME TO THE JUNGLE</p>
-          </div>
-
-        </div>
-
-        <div>
-          <div className="flex flex-col justify-start items-start bg-blue-100 rounded p-6 mb-4">
-            <p className="mb-4">This podcast is awesome blah blah blah Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi, nulla debitis vero assumenda voluptas fugiat cupiditate iusto exercitationem minus eaque.</p>
-            <p className="mb-4">- Person</p>
-          </div>
-
-          <div className="flex flex-col justify-start items-start bg-blue-100 rounded p-6 mb-4">
-            <p>"HELLO!"</p>
-            <p>WLECOME TO THE JUNGLE</p>
-          </div>
-        </div>
-
-        <div>
-          <div className="flex flex-col justify-start items-start bg-blue-100 rounded p-6 mb-4">
-            <p className="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam deleniti placeat ab unde eveniet corrupti, sit eius beatae dolor nam dignissimos, delectus fuga? Praesentium obcaecati adipisci culpa accusamus officia tempore?</p>
-            <p className="mb-4">- Joe</p>
-          </div>
-
-          <div className="flex flex-col justify-center items-center bg-blue-100 rounded p-6 mb-4">
-            <p >"HELLO!"</p>
-            <p>WLECOME TO THE JUNGLE</p>
-          </div>
+          <p>UNDER MAINTENANCE</p>
 
         </div>
       </div>
